b6aa0901aa16958471e4be54ad6ee3fe1a639a06
pierreyoda <pierreyoda33@gmail.com>
2015-06-30 15:52:03 +0200

    Step 0

--- src/bin/step0_repl.rs
+++ src/bin/step0_repl.rs
@@ -1,3 +1,32 @@
+use std::io;
+use std::io::Write;
+
+fn read(string: String) -> String {
+    string
+}
+
+fn eval(ast: String) -> String {
+    ast
+}
+
+fn print(expr: String) -> String {
+    expr
+}
+
+fn rep(string: &str) -> String {
+    print(eval(read(string.to_string())))
+}
+
 fn main() {
+    let input = &mut String::new();
+    'repl: loop {
+        input.clear();
+        print!("user> ");
+        io::stdout().flush().ok().expect("output error");
+        io::stdin().read_line(input)
+            .ok().expect("input : failed to read line");
+        println!("{}", rep(input));
+    }
+
     println!("Hello, world!");
 }
