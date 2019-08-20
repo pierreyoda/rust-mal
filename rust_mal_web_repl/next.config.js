306d0e73938204a36c7d31f9bd89c7aa9565f2c6
pierreyoda <pierreyoda@users.noreply.github.com>
2019-08-20 22:22:47 +0200

    :construction: WebAssembly basic setup with wasm-pack & NextJS

---
+++ rust_mal_web_repl/next.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = () => ({
+    webpack: config => ({
+        plugins: [
+            ...config.plugins,
+            new WasmPackPlugin({
+                crateDirectory: path.resolve(__dirname, "../rust_mal_lib_wasm"),
+
+                // Check https://rustwasm.github.io/wasm-pack/book/commands/build.html for
+                // the available set of arguments.
+                //
+                // Default arguments are `--typescript --target browser --mode normal`.
+                extraArgs: "--no-typescript",
+
+                // Optional array of absolute paths to directories, changes to which
+                // will trigger the build.
+                // watchDirectories: [
+                //   path.resolve(__dirname, "another-crate/src")
+                // ],
+
+                // The same as the `--out-dir` option for `wasm-pack`
+                // outDir: "pkg",
+
+                // The same as the `--out-name` option for `wasm-pack`
+                // outName: "index",
+
+                // If defined, `forceWatch` will force activate/deactivate watch mode for
+                // `.rs` files.
+                //
+                // The default (not set) aligns watch mode for `.rs` files to Webpack's
+                // watch mode.
+                // forceWatch: true,
+
+                // If defined, `forceMode` will force the compilation mode for `wasm-pack`
+                //
+                // Possible values are `development` and `production`.
+                //
+                // the mode `development` makes `wasm-pack` build in `debug` mode.
+                // the mode `production` makes `wasm-pack` build in `release` mode.
+                // forceMode: "development",
+            }),
+        ],
+    }),
+});
