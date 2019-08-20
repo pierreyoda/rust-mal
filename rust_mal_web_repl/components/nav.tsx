578702a2299640eb10cee0d3e62a31e7477b7ec1
pierreyoda <pierreyoda@users.noreply.github.com>
2019-08-20 23:29:03 +0200

    :wrench: Web REPL: Typescript support

--- rust_mal_web_repl/components/nav.tsx
+++ rust_mal_web_repl/components/nav.tsx
@@ -1,19 +1,19 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' }
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+  { href: "https://zeit.co/now", label: "ZEIT" },
+  { href: "https://github.com/zeit/next.js", label: "GitHub" }
+].map(link => ({
+    ...link,
+    key: `nav-link-${link.href}-${link.label}`,
+}));
 
 const Nav = () => (
   <nav>
     <ul>
       <li>
-        <Link href='/'>
+        <Link href="/">
           <a>Home</a>
         </Link>
       </li>
@@ -53,4 +53,4 @@ const Nav = () => (
   </nav>
 )
 
-export default Nav
+export default Nav;
---
+++ rust_mal_web_repl/pages/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Head from "next/head";
+import { NextPage } from "next";
+
+import Nav from "../components/nav";
+
+const Home: NextPage<{ rustMessage: string }> = ({ rustMessage }) => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <Nav />
+    <div className="hero">
+      <h1 className="title">Welcome to Next.js!</h1>
+      <p className="description">
+        Hello from Rust: <code>{rustMessage}</code> \o/
+    </p>
+    </div>
+    <style jsx>{`
+    .hero {
+      width: 100%;
+      color: #333;
+    }
+    .title {
+      margin: 0;
+      width: 100%;
+      padding-top: 80px;
+      line-height: 1.15;
+      font-size: 48px;
+    }
+    .title,
+    .description {
+      text-align: center;
+    }
+  `}</style>
+  </div>
+);
+
+Home.getInitialProps = async () => {
+    const { greet } = await import("../../rust_mal_lib_wasm/pkg");
+    return { rustMessage: greet() };
+};
+
+export default Home;
